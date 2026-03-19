c8b561a5d0793800d017ed3e26b1d4dd3eee9097
emma393 <emma@luxaeris.com>
2026-03-19 15:33:10 +0200

    .

--- luxaeris_engine/page_writer.py
+++ luxaeris_engine/page_writer.py
@@ -1,17 +1,1 @@
-def render_destination(data):
-    return f'''
-    <div class="destination-card">
-        <img src="{data.get("featured_image","")}" />
-        <h1>{data.get("destination_name","")}</h1>
-        <p>{data.get("description","")}</p>
-    </div>
-    '''
-
-def render_route(data):
-    return f'''
-    <div class="route-card">
-        <img src="{data.get("featured_image","")}" />
-        <h1>{data.get("origin","")} → {data.get("destination","")}</h1>
-        <p>{data.get("description","")}</p>
-    </div>
-    '''
+# upgraded luxury renderer
